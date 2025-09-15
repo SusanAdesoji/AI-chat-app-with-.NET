8b7810e59dbfe877227a924b63cbf004c78b7a14
Susan Adesoji <Adesojisuzan@gmail.com>
2025-09-15 05:21:24 +0000

    Initial commit (clean repo, no secrets)

---
+++ ExtensionsAI/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+string? model = config["ModelName"];
+string? key = config["OpenAIKey"];
+
+IChatClient client =
+    new OpenAIClient(key).GetChatClient(model).AsIChatClient();
+
+string text = File.ReadAllText("benefits.md");
+string prompt = $"""
+    Summarize the the following text in 20 words or less:
+    {text}
+    """;
+
+
+// Submit the prompt and print out the response.
+ChatResponse response = await client.GetResponseAsync(
+    prompt,
+    new ChatOptions { MaxOutputTokens = 400 });
+Console.WriteLine(response);
